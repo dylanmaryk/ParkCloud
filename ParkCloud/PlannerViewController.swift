a4220ed68b27860b2114441e0995b4600a534da1
Dylan Maryk <dylan@door2door.io>
2017-03-04 16:36:44 +0100

    Add more views.

---
+++ ParkCloud/PlannerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PlannerViewController.swift
+//  ParkCloud
+//
+//  Created by Dylan Maryk on 04/03/2017.
+//  Copyright © 2017 ParkCloud. All rights reserved.
+//
+
+import UIKit
+
+class PlannerViewController: UIViewController {
+    
+    @IBOutlet var mapView: NMAMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
