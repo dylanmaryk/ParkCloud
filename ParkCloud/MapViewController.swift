8215df532cc4fb8d8c272fb957ca3da156a15963
Dylan Maryk <dylan@door2door.io>
2017-03-05 02:41:07 +0100

    Add routes to routes view. Add alert view.

---
+++ ParkCloud/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewController.swift
+//  ParkCloud
+//
+//  Created by Dylan Maryk on 05/03/2017.
+//  Copyright © 2017 ParkCloud. All rights reserved.
+//
+
+import GoogleMaps
+import UIKit
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var drivingToLabel: UILabel!
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    var destination: String!
+    var polylineString: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.drivingToLabel.text = "Driving to \(self.destination!)"
+        
+        let polyline = GMSPolyline(path: GMSPath(fromEncodedPath: polylineString))
+        polyline.map = self.mapView
+        
+        self.mapView.animate(toLocation: polyline.path!.coordinate(at: 0))
+        self.mapView.animate(toZoom: 13)
+    }
+}
--- ParkCloud/RouteViewController.swift
+++ ParkCloud/RouteViewController.swift
@@ -9,14 +9,22 @@
 import GoogleMaps
 import UIKit
 
-class RouteViewController: UIViewController {
+class RouteCell: UITableViewCell {
+    @IBOutlet weak var endAddressLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+}
+
+class RouteViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var drivingToLabel: UILabel!
     
     @IBOutlet weak var mapView: GMSMapView!
     
+    @IBOutlet weak var tableView: UITableView!
+    
     var destination: String!
     var destinationLocation: (lat: Float, lng: Float)!
     var routes: [Route]!
+    var routeSelected: Route!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,5 +34,41 @@ class RouteViewController: UIViewController {
         self.mapView.animate(toLocation: CLLocationCoordinate2D(latitude: CLLocationDegrees(self.destinationLocation.lat),
                                                                 longitude: CLLocationDegrees(self.destinationLocation.lng)))
         self.mapView.animate(toZoom: 14)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var identifier: String
+        
+        if indexPath.row == 0 {
+            identifier = "RouteCell"
+        } else if indexPath.row == 1 {
+            identifier = "RouteCell2"
+        } else {
+            identifier = "RouteCell3"
+        }
+        
+        let routeCell = self.tableView.dequeueReusableCell(withIdentifier: identifier) as! RouteCell
+        routeCell.selectionStyle = .none
+        
+        let route = routes[indexPath.row]
+        routeCell.endAddressLabel.text = route.endAddress
+        routeCell.durationLabel.text = route.duration
+        
+        return routeCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.routeSelected = self.routes[indexPath.row]
+        self.performSegue(withIdentifier: "showAlert", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let alertVC = segue.destination as! AlertViewController
+        alertVC.destination = self.destination
+        alertVC.polylineString = self.routeSelected.polyline
     }
 }
